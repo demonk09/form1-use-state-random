c4eae44f18b51045ce47f2a791d62123f4f95b51
Mayur Kamble <54568942+demonk09@users.noreply.github.com>
2023-12-15 12:47:40 +0530

    random-number

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import "./App.css"
 
-function App() {
+export default function App() {
+  let [c,setc]=useState(0)
+  let inc=()=>{
+    setc(c+1)
+  }
+  let dec=()=>{
+    setc(c-1)
+  }
+  let [c1,setc1]=useState(0)
+  let ran=()=>{
+    setc1(Math.round(Math.random(c)*100))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='div1'>
+          
+      
+       <h1 onClick={dec} >-</h1>
+      <h1 className='count'>{c}</h1>
+     
+      <h1 onClick={inc} >+</h1>
     </div>
-  );
-}
+    <div className='ran'>
 
-export default App;
+    <h1 className='count'>{c1}</h1>
+    <button onClick={ran}>Random</button>
+
+    </div>
+    </div>
+  )
+}
